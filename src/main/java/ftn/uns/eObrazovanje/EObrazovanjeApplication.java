cfa0b7de1255ac643a534d2ea3e752e8646c353c
StefanDavidovic <davidovic.sf15.2019@uns.ac.rs>
2022-05-29 17:48:30 +0200

    Added functionality for upload files for students and dowdload them

--- src/main/java/ftn/uns/eObrazovanje/EObrazovanjeApplication.java
+++ src/main/java/ftn/uns/eObrazovanje/EObrazovanjeApplication.java
@@ -1,5 +1,7 @@
 package ftn.uns.eObrazovanje;
 
+import org.springframework.boot.CommandLineRunner;
+import javax.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,23 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class EObrazovanjeApplication {
+import ftn.uns.eObrazovanje.service.FilesStorageService;
 
+@SpringBootApplication
+public class EObrazovanjeApplication implements CommandLineRunner{
+	@Resource
+	FilesStorageService storageService;	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EObrazovanjeApplication.class, args);
 	}
+
+	@Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
+	
+	
 
 }
